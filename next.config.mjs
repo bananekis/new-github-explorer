55561f694847a74a6399085bf51f6ca96834acea
Lukas Novak <nlukas187@gmail.com>
2024-09-14 17:09:00 +0200

    feat: remove build time envs

--- next.config.mjs
+++ next.config.mjs
@@ -3,11 +3,6 @@ const nextConfig = {
 	experimental: {
 		serverActions: true,
 	},
-	env: {
-		NEXT_PUBLIC_API_URL: process.env.VERCEL_URL
-			? `https://${process.env.VERCEL_URL}`
-			: "http://localhost:3000",
-	},
 };
 
 export default nextConfig;
